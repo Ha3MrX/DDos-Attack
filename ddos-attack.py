12e79aff89cef9cd0e895c3970dcc5330804468e
Ethan <40130389+EthanLeitch@users.noreply.github.com>
2018-09-18 13:21:28 +1200

    Update ddos-attack.py

--- ddos-attack.py
+++ ddos-attack.py
@@ -30,15 +30,7 @@ port = input("Port       : ")
 
 os.system("clear")
 os.system("figlet Attack Starting")
-print "[                    ] 0% "
-time.sleep(5)
-print "[=====               ] 25%"
-time.sleep(5)
-print "[==========          ] 50%"
-time.sleep(5)
-print "[===============     ] 75%"
-time.sleep(5)
-print "[====================] 100%"
+#removed unneccesary loading bar
 time.sleep(3)
 sent = 0
 while True:

